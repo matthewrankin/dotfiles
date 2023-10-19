b8958696bbcbb40fd0f69cfa8ccab5beadf3ff93
Matthew Rankin <matthew@cumulusware.com>
2023-10-19 15:56:29 -0500

    Small mods to gitsigns, harpoon & nvim-colorizer

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/gitsigns.lua
@@ -1,6 +1,5 @@
 return {
   'lewis6991/gitsigns.nvim',
-  config = function()
-      require("gitsigns").setup()
-    end,
+  event = { "BufReadPre", "BufNewFile" },
+  config = true,
 }
--- dotfiles/.config/nvim/lua/matthewrankin/plugins/harpoon.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/harpoon.lua
@@ -1,5 +1,8 @@
 return {
   'theprimeagen/harpoon',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
