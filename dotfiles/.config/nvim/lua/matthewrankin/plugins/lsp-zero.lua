92cb0124e2573cd3e2d55dfe9b3809fc136deec1
Matthew Rankin <matthew@cumulusware.com>
2023-10-20 14:05:47 -0500

    Refactor lsp-zero

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp-zero.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp-zero.lua
@@ -20,23 +20,23 @@ return {
     {'rafamadriz/friendly-snippets'},
   },
   config = function()
-    local lsp = require('lsp-zero')
-    lsp.preset('recommended')
+    local lsp_zero = require('lsp-zero')
+    lsp_zero.preset('recommended')
     local cmp = require('cmp')
     local cmp_select = {behavior = cmp.SelectBehavior.Select}
-    local cmp_mappings = lsp.defaults.cmp_mappings({
+    local cmp_mappings = lsp_zero.defaults.cmp_mappings({
       ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
       ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
       ['<C-space>'] = cmp.mapping.complete(),
     })
-    lsp.set_preferences({
+    lsp_zero.set_preferences({
       sign_icons = { }
     })
 
-    lsp.on_attach(function(client, bufnr)
+    lsp_zero.on_attach(function(client, bufnr)
       local opts = {buffer = bufnr, remap = false}
-      lsp.buffer_autoformat()
+      lsp_zero.buffer_autoformat()
 
       vim.keymap.set("n", "gd",
         function() vim.lsp.buf.definition() end, opts)
@@ -60,7 +60,17 @@ return {
         function() vim.lsp.buf.signature_help() end, opts)
     end)
 
-    lsp.setup()
+    lsp_zero.format_on_save({
+      format_opts = {
+        async = false,
+        timeout_ms = 10000,
+      },
+      servers = {
+        ['tsserver'] = {'javascript', 'typescript'},
+      }
+    })
+
+    lsp_zero.setup()
 
     vim.diagnostic.config({
         virtual_text = true
