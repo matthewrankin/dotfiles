5d40e8a5d08e8e8744e4dbd794f22470affbeacb
Matthew Rankin <matthew@cumulusware.com>
2023-10-23 10:22:10 -0500

    Modify nvim-surround config

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-surround.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-surround.lua
@@ -2,9 +2,5 @@ return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` for latest features
   event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
-  end
+  config = true,
 }
