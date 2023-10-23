1d67b342d20f9cbbfd4e2c827341a0db9448cf48
Matthew Rankin <matthew@cumulusware.com>
2023-10-23 07:48:37 -0500

    Add checker and change_detection to lazy.lua

--- dotfiles/.config/nvim/lua/matthewrankin/lazy.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('matthewrankin.plugins')
+require("lazy").setup("matthewrankin.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
