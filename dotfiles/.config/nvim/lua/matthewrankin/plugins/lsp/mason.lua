3d0dc046a1bfe7fccf69883386e3f1535b4e91f7
Matthew Rankin <matthew@cumulusware.com>
2023-10-25 11:58:41 -0500

    Use mason-tool-installer.nvim

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/mason.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup({
 			ui = {
@@ -34,6 +36,20 @@ return {
 				"pyright", -- Python
 			},
 			automatic_installation = true,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+				"flake8",
+				"goimports",
+				"golines",
+			},
 		})
 	end,
 }
