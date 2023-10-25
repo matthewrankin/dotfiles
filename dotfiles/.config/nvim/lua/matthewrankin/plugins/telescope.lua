dbba00ded4e803e502ecaee45f6c1aa2a52af0f6
Matthew Rankin <matthew@cumulusware.com>
2023-10-25 14:04:06 -0500

    Change telescope to vertical layout strategy

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/telescope.lua
@@ -1,51 +1,50 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          }
-        }
-      }
-    })
+		telescope.setup({
+			defaults = {
+				layout_strategy = "vertical",
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf");
+		telescope.load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap
-    local builtin = require("telescope.builtin")
+		-- set keymaps
+		local keymap = vim.keymap
+		local builtin = require("telescope.builtin")
 
-    keymap.set("n", "<leader>ff", builtin.find_files, {
-      desc = "Search for files respecting .gitignore",
-    })
-    keymap.set("n", "<leader>fr", builtin.oldfiles, {
-      desc = "Fuzzy find recent files",
-    })
-    keymap.set("n", "<C-p>", builtin.git_files, {
-      desc = "Fuzzy search for files tracked by git respecting .gitignore",
-    })
-    keymap.set("n", "<leader>fs", builtin.live_grep, {
-      desc = "Find string",
-    })
-    keymap.set("n", "<leader>fc", builtin.grep_string, {
-      desc = "Find string under cursor",
-    })
-    keymap.set("n", "<leader>fb", builtin.buffers, {
-    })
-    keymap.set("n", "<leader>fh", builtin.help_tags, {
-    })
-  end,
+		keymap.set("n", "<leader>ff", builtin.find_files, {
+			desc = "Search for files respecting .gitignore",
+		})
+		keymap.set("n", "<leader>fr", builtin.oldfiles, {
+			desc = "Fuzzy find recent files",
+		})
+		keymap.set("n", "<C-p>", builtin.git_files, {
+			desc = "Fuzzy search for files tracked by git respecting .gitignore",
+		})
+		keymap.set("n", "<leader>fs", builtin.live_grep, {
+			desc = "Find string",
+		})
+		keymap.set("n", "<leader>fc", builtin.grep_string, {
+			desc = "Find string under cursor",
+		})
+		keymap.set("n", "<leader>fb", builtin.buffers, {})
+		keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	end,
 }
