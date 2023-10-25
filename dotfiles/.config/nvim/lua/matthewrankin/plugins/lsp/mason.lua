b771e2d3ca11a99510985f2c811a935d7c5e1d13
Matthew Rankin <matthew@cumulusware.com>
2023-10-25 11:50:31 -0500

    Add linting and formatting to neovim

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/mason.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/mason.lua
@@ -1,38 +1,39 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        }
-      }
-    })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "html",             -- HTML
-        "cssls",            -- CSS
-        "tailwindcss",      -- Tailwind CSS
-        "tsserver",         -- JavaScript/TypeScript
-        "jsonls",           -- JSON
-        "marksman",         -- Markdown
-        "golangci_lint_ls", -- Go LSP
-        "gopls",            -- Gopls
-        "ltex",             -- LaTeX
-        "svelte",           -- Svelte
-        "lua_ls",           -- Lua
-        "emmet_ls",         -- Emmet HTML/CSS snippets
-        "pyright",          -- Python
-      },
-      automatic_installation = true,
-    })
-  end,
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"html", -- HTML
+				"cssls", -- CSS
+				"tailwindcss", -- Tailwind CSS
+				"tsserver", -- JavaScript/TypeScript
+				"jsonls", -- JSON
+				"marksman", -- Markdown
+				"golangci_lint_ls", -- Go LSP
+				"gopls", -- Gopls
+				"ltex", -- LaTeX
+				"svelte", -- Svelte
+				"lua_ls", -- Lua
+				"emmet_ls", -- Emmet HTML/CSS snippets
+				"pyright", -- Python
+			},
+			automatic_installation = true,
+		})
+	end,
 }
