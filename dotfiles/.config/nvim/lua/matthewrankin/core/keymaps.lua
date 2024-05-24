601bcfe74002907e941d9be3d2ab88b458e7f5ac
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-05-24 07:46:15 -0500

    Add some neovim keymaps

--- dotfiles/.config/nvim/lua/matthewrankin/core/keymaps.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/core/keymaps.lua
@@ -1,63 +1,92 @@
 -- Set leader key
 vim.g.mapleader = " "
 
-vim.keymap.set(
-  "n",
-  "<leader>nh",
-  ":nohl<CR>",
-  { desc = "Clear search highlights" }
-)
+local keymap = vim.keymap -- for conciseness
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Allow moving lines when visually selected.
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Keep cursor at beginning of line when joining lines.
-vim.keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "J", "mzJ`z")
 
 -- Keep cursor in middle when half page jumping
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Keep cursor in middle when searching
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
 -- Greatest remap ever. Delete highlighted word into the void register and then
 -- paste, so that we keep our paste the same in the future. No longer needed as
 -- of Vim 8.2.4881 and Neovim v0.8.0, as you can paste using P for the same
 -- action.
-vim.keymap.set("x", "<leader>p", '"_dP')
+keymap.set("x", "<leader>p", '"_dP')
 
 -- Next greatest remap ever. Yank into the system clipboard using <leader>y in
 -- either normal or visual mode or in normal mode yank to the end of the line
 -- using <leader>Y.
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
+keymap.set({ "n", "v" }, "<leader>y", '"+y')
+keymap.set("n", "<leader>Y", '"+Y')
 
 vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
 -- Never press Q
-vim.keymap.set("n", "Q", "<nop>")
+keymap.set("n", "Q", "<nop>")
 
--- Need to find a different keymap for this command, as I don't want to
--- overwrite the typical usage of <C-f>.
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>x", "<cmd>lprev<CR>zz")
+keymap.set("n", "<leader>s", "<cmd>lnext<CR>zz")
+keymap.set("n", "<leader>x", "<cmd>lprev<CR>zz")
 
 -- Change the current word and all matches
-vim.keymap.set(
+keymap.set(
   "n",
   "<leader>s",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
 )
 
 -- Make file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set(
+  "n",
+  "<leader>sx",
+  "<cmd>close<CR>",
+  { desc = "Close current split" }
+) -- close current split window
+
+-- tab management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set(
+  "n",
+  "<leader>tx",
+  "<cmd>tabclose<CR>",
+  { desc = "Close current tab" }
+) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set(
+  "n",
+  "<leader>tf",
+  "<cmd>tabnew %<CR>",
+  { desc = "Open current buffer in new tab" }
+) --  move current buffer to new tab
