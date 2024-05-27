439d5d8f64f41e5aa4169a8c9d2a5a88ef00b91b
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-05-27 07:29:24 -0500

    Update to Harpoon 2

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/harpoon.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/harpoon.lua
@@ -1,29 +1,41 @@
 return {
   "theprimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local harpoon = require("harpoon")
+    harpoon:setup()
 
-    vim.keymap.set("n", "<leader>a", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { desc = "Add file to Harpoon" })
+
+    -- Note: Overrides the default <C-e> behavior, which is to scroll down one
+    -- line.
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Toggle Harpoon Menu" })
 
     vim.keymap.set("n", "<leader>f1", function()
-      ui.nav_file(1)
-    end)
+      harpoon:list():select(1)
+    end, { desc = "Harpoon File #1" })
+
     vim.keymap.set("n", "<leader>f2", function()
-      ui.nav_file(2)
-    end)
+      harpoon:list():select(2)
+    end, { desc = "Harpoon File #2" })
+
     vim.keymap.set("n", "<leader>f3", function()
-      ui.nav_file(3)
-    end)
+      harpoon:list():select(3)
+    end, { desc = "Harpoon File #3" })
+
     vim.keymap.set("n", "<leader>f4", function()
-      ui.nav_file(4)
-    end)
+      harpoon:list():select(4)
+    end, { desc = "Harpoon File #4" })
+
     vim.keymap.set("n", "<leader>f5", function()
-      ui.nav_file(5)
-    end)
+      harpoon:list():select(5)
+    end, { desc = "Harpoon File #5" })
   end,
 }
