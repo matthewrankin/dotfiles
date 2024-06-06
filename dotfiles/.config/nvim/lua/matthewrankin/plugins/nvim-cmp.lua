339ec56b33b295bbe88f7f3ad885ec5947dc6f26
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-06-06 07:05:32 -0600

    Add keymap to jump to next/prev luasnip

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-cmp.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-cmp.lua
@@ -52,5 +52,18 @@ return {
         }),
       },
     })
+
+    -- Luasnip jumping
+    vim.keymap.set({ "i", "s" }, "<C-L>", function()
+      luasnip.jump(1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-J>", function()
+      luasnip.jump(-1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-E>", function()
+      if luasnip.choice_active() then
+        luasnip.change_choice(1)
+      end
+    end, { silent = true })
   end,
 }
