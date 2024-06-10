d871c7713da8508ef9ed8677297131e33e8fb5d6
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-06-10 06:17:01 -0500

    Add folke/neodev.nvim to lspconfig.lua

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", opts = {} },
   },
   config = function()
     local lspconfig = require("lspconfig")
