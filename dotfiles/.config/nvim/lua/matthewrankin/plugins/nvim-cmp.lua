84845e1bddc48ea701920824a434a9e2e00c6c3f
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-06-12 03:36:01 -0500

    nvim-cmp updates

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-cmp.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/nvim-cmp.lua
@@ -1,19 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
+  lazy = false,
+  priority = 100,
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "onsails/lspkind.nvim", -- vs-code like pictograms
+    "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-buffer", -- source for text in buffer
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
   },
   config = function()
     local cmp = require("cmp")
-
     local luasnip = require("luasnip")
-
     local lspkind = require("lspkind")
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
@@ -23,11 +24,14 @@ return {
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
+
+      -- Enable luasnip to handle snippet expansion for nvim-cmp
+      snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -35,15 +39,15 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
-      -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
+
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
@@ -54,13 +58,13 @@ return {
     })
 
     -- Luasnip jumping
-    vim.keymap.set({ "i", "s" }, "<C-L>", function()
+    vim.keymap.set({ "i", "s" }, "<C-j>", function()
       luasnip.jump(1)
     end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-J>", function()
+    vim.keymap.set({ "i", "s" }, "<C-k>", function()
       luasnip.jump(-1)
     end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-E>", function()
+    vim.keymap.set({ "i", "s" }, "<C-e>", function()
       if luasnip.choice_active() then
         luasnip.change_choice(1)
       end
