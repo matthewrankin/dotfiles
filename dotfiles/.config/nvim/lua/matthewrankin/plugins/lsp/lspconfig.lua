6a8daf244f706da82cff5379428bafc28f8e0531
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-09-06 11:07:49 -0500

    Replace tsserver with ts_ls

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
@@ -98,7 +98,7 @@ return {
       filetypes = { "html", "temp" },
     })
 
-    lspconfig.tsserver.setup({
+    lspconfig.ts_ls.setup({
       capabilities = capabilities,
       on_attach = on_attach,
     })
