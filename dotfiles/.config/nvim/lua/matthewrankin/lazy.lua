ad1ec24106bb97f849f1c3c072f2216407f9d66b
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-12-25 09:50:02 -0600

    Update Lazy.nvim from v11 to v14

--- dotfiles/.config/nvim/lua/matthewrankin/lazy.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/lazy.lua
@@ -1,25 +1,52 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
+  local lazyrepo = "https://github.com/LazyVim/lazy.nvim.git"
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
+    lazyrepo,
     lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "matthewrankin.plugins" },
-  { import = "matthewrankin.plugins.lsp" },
-}, {
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "matthewrankin.plugins" },
+    { import = "matthewrankin.plugins.lsp" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
   checker = {
     enabled = true,
     notify = false,
   },
-  change_detection = {
-    notify = false,
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
