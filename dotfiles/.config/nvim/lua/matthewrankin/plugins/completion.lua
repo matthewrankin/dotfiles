dc9ca5a0521d4258166581c1f046e5ef1250f68c
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-12-26 07:02:44 -0600

    Use C-k and C-j for prev/next nvim completions

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/completion.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/completion.lua
@@ -1,24 +1,19 @@
 return {
   "saghen/blink.cmp",
-  -- optional: provides snippets for the snippet source
   dependencies = "rafamadriz/friendly-snippets",
 
   -- use a release tag to download pre-built binaries
   version = "*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
 
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+    },
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
       -- Useful for when your theme doesn't support blink.cmp
