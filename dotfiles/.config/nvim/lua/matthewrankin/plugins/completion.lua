d58faff1c2eaa26709cb8105375bd5c93d7303e3
Matthew Rankin <matthewrankin@users.noreply.github.com>
2024-12-27 06:37:21 -0600

    Modify completion.lua

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/completion.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/completion.lua
@@ -1,12 +1,6 @@
 return {
   "saghen/blink.cmp",
   dependencies = "rafamadriz/friendly-snippets",
-
-  -- use a release tag to download pre-built binaries
-  version = "*",
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
     keymap = {
       preset = "default",
@@ -15,20 +9,13 @@ return {
       ["<C-j>"] = { "select_next", "fallback" },
     },
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
       use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
     },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
+    signature = { enabled = true },
   },
   opts_extend = { "sources.default" },
 }
