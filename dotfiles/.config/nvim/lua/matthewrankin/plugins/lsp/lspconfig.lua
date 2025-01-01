264a5eaa9d3534807969dd0e00d589e6683992cf
Matthew Rankin <matthewrankin@users.noreply.github.com>
2025-01-01 07:11:33 -0600

    Modify lspconfig.lua
    I'm still playing with whether or not I truly want to use blink.cmp or
    cmp.nvim

--- dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
+++ dotfiles/.config/nvim/lua/matthewrankin/plugins/lsp/lspconfig.lua
@@ -1,7 +1,36 @@
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "saghen/blink.cmp" },
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    { -- optional blink completion source for require statements and module annotations
+      "saghen/blink.cmp",
+      opts = {
+        sources = {
+          -- add lazydev to your completion providers
+          default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+          providers = {
+            lazydev = {
+              name = "LazyDev",
+              module = "lazydev.integrations.blink",
+              -- make lazydev completions top priority (see `:h blink.cmp`)
+              score_offset = 100,
+            },
+          },
+        },
+      },
+    },
+  },
   config = function()
     local lspconfig = require("lspconfig")
     local capabilities = require("blink.cmp").get_lsp_capabilities()
