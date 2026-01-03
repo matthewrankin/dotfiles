51190ad1d1fa5fc808f68dbfa101447a99bb8bb8
Matthew Rankin <matthewrankin@users.noreply.github.com>
2026-01-03 07:25:02 -0600

    Update repo link for lazy.nvim

--- nvim/lua/matthewrankin/lazy.lua
+++ nvim/lua/matthewrankin/lazy.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/LazyVim/lazy.nvim.git"
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({
     "git",
     "clone",
@@ -15,10 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
       { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
+    vim.fn.getchar()
     os.exit(1)
   end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
